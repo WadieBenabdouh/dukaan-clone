4fd2752b41d3fee1580611aeb00a70c42dd45807
Wadie Benabdouh <wadie.trade@gmail.com>
2023-12-21 12:28:30 +0100

    main?

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ let mobileMenu = document.getElementById("mobileMenu");
 
 // Burger Menu Utilities
 mobileMenuBtn.onclick = () => {
-    mobileMenuBtn.classList.toggle("is-active")
-    mobileMenu.classList.toggle("is-active")
+    mobileMenuBtn.classList.toggle("is-active");
+    mobileMenu.classList.toggle("is-active");
 }
 // Burger Menu Utilities end
+
+// Mobile Menu links
+let dropdownTitleOne = document.getElementById("dropdownTitleOne");
+let dropdownOne = document.getElementsById("dropdown-one");
+
+dropdownTitleOne.onclick = () => {
+    dropdownOne.classList.toggle("is-called");
+}
+
+// Mobile Menu link End
