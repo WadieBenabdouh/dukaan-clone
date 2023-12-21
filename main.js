3f7ae6f8fa283cd9f9aa8fe0a3e61550c8281329
Wadie Benabdouh <wadie.trade@gmail.com>
2023-12-21 10:42:11 +0100

    added burger menu

---
+++ main.js
@@ -0,0 +1,8 @@
+let mobileMenuBtn = document.getElementById("mobileMenuBtn");
+let mobileMenu = document.getElementById("mobileMenu");
+
+// Burger Menu Utilities
+mobileMenuBtn.onclick = () => {
+    mobileMenuBtn.classList.toggle("is-active")
+}
+// Burger Menu Utilities end
