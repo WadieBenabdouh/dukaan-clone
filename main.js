c1d0ff1867213c06e73162028418d04059af12ff
Wadie Benabdouh <wadie.trade@gmail.com>
2023-12-21 12:41:26 +0100

    all three dropdowns are working using JS Sauce

--- main.js
+++ main.js
@@ -11,11 +11,31 @@ mobileMenuBtn.onclick = () => {
 // Burger Menu Utilities end
 
 // Mobile Menu links
+// first dropdown
 let dropdownTitleOne = document.getElementById("dropdownTitleOne");
 let dropdownOne = document.getElementById("dropdown-one");
 
 dropdownTitleOne.onclick = () => {
     dropdownOne.classList.toggle("is-called");
 }
+
+
+// second dropdown
+let dropdownTitleTwo = document.getElementById("dropdownTitleTwo");
+let dropdownTwo = document.getElementById("dropdown-two");
+
+dropdownTitleTwo.onclick = () => {
+    dropdownTwo.classList.toggle("is-called");
+}
+
+
+// third dropdown
+let dropdownTitleThree = document.getElementById("dropdownTitleThree");
+let dropdownThree = document.getElementById("dropdown-three");
+
+dropdownTitleThree.onclick = () => {
+    dropdownThree.classList.toggle("is-called");
+}
+
 
 // Mobile Menu link End
