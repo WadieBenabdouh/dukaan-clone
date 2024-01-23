1f6ff75492e4afb5624b945def60f22af88b547c
Wadie Benabdouh <wadie.trade@gmail.com>
2024-01-23 12:41:25 +0100

    impressive work.

--- main.js
+++ main.js
@@ -40,5 +40,52 @@ dropdownTitleThree.onclick = () => {
 
 // BRANDS AUTO SLIDER
 let clonedImages = document.querySelector(".brands-slider").cloneNode(true);
-document.querySelector(".brands").appendChild(clonedImages)
+document.querySelector(".brands").appendChild(clonedImages);
 // BRANDS AUTO SLIDER END
+
+
+
+// ---- Feedback Dots Slider
+let btnOne = document.getElementById("btn-one");
+let btnTwo = document.getElementById("btn-two");
+let btnThree = document.getElementById("btn-three");
+let btnFour = document.getElementById("btn-four");
+let testimonialPusherAgent = document.getElementById("testimonial-one-pusher");
+console.log(testimonialPusherAgent);
+
+btnOne.onclick = () => {
+    testimonialPusherAgent.style.marginLeft = "40%"
+    btnOne.style.fontWeight = "bold";
+
+    btnTwo.style.fontWeight = "normal";
+    btnThree.style.fontWeight = "normal";
+    btnFour.style.fontWeight = "normal";
+
+}
+
+btnTwo.onclick = () => {
+    testimonialPusherAgent.style.marginLeft = "20%"
+    btnTwo.style.fontWeight = "bold";
+
+    btnOne.style.fontWeight = "normal";
+    btnThree.style.fontWeight = "normal";
+    btnFour.style.fontWeight = "normal";
+}
+btnThree.onclick = () => {
+    testimonialPusherAgent.style.marginLeft = "0%"
+    btnThree.style.fontWeight = "bold";
+
+    btnOne.style.fontWeight = "normal";
+    btnTwo.style.fontWeight = "normal";
+    btnFour.style.fontWeight = "normal";
+}
+
+btnFour.onclick = () => {
+    testimonialPusherAgent.style.marginLeft = "-20%"
+    btnFour.style.fontWeight = "bold";
+
+    btnOne.style.fontWeight = "normal";
+    btnTwo.style.fontWeight = "normal";
+    btnThree.style.fontWeight = "normal";
+}
+// ---- Feedback Dots Slider END
